ed8462cf96efa63a992ae2d56ed57c68db96e3e4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:02:19 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "function_app_flex_consumptions_id" {
+  description = "Map of id values across all function_app_flex_consumptions, keyed the same as var.function_app_flex_consumptions"
+  value       = { for k, v in azurerm_function_app_flex_consumption.function_app_flex_consumptions : k => v.id }
+}
 output "function_app_flex_consumptions_always_ready" {
   description = "Map of always_ready values across all function_app_flex_consumptions, keyed the same as var.function_app_flex_consumptions"
   value       = { for k, v in azurerm_function_app_flex_consumption.function_app_flex_consumptions : k => v.always_ready }
